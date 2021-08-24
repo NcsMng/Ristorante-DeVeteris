32e8c57254e5047faf85abbab6c13dcf395c5b5f
marian.neacsu <marian.neacsu@soft.it>
2021-08-24 11:32:46 +0200

    Creazione Permessi

---
+++ permessi/src/main/java/permessi/PermessiApplication.java
@@ -0,0 +1,13 @@
+package permessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermessiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermessiApplication.class, args);
+	}
+
+}
---
+++ permessi/src/test/java/permessi/PermessiApplicationTests.java
@@ -0,0 +1,13 @@
+package permessi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermessiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
